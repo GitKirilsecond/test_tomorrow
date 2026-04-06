3092699cb9758d4130015989c697b3a2626f1e81
Kiril <kiril.strykalenko@icloud.com>
2026-04-06 21:26:28 +0200

    new type test2

--- test.py
+++ test.py
@@ -1,39 +1,44 @@
 import requests
-from datetime import datetime, timedelta
+import time
+from datetime import datetime
 from zoneinfo import ZoneInfo
 
 TOKEN = "8581090817:AAFC1bzXTfJvqHHxFmPzfXECciSjrErbyjM"
 CHAT_USERNAME = "@asdqwesszx"
-MESSAGE = "Зупинімося на мить, щоб вшанувати пам’ять Героїв, які поклали своє життя заради нашого майбутнього. Їхній подвиг назавжди залишиться в нашій пам’яті. \n\nВічна Слава і вдячність."
+MESSAGE = "ТЕСТ 22:00"
 PHOTO_PATH = "photo.png"
 
-def send_scheduled_message():
-    # Європейський час (Данія)
-    now = datetime.now(ZoneInfo("Europe/Copenhagen"))
-
-    # ставимо на завтра 09:00
-    target = now.replace(
-        hour=9,
-        minute=0,
-        second=0,
-        microsecond=0
-    ) + timedelta(days=1)
-
-    timestamp = int(target.timestamp())
-
+def send_message():
     url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
-
     with open(PHOTO_PATH, "rb") as photo_file:
         payload = {
             "chat_id": CHAT_USERNAME,
-            "caption": MESSAGE,
-            "schedule_date": timestamp
+            "caption": MESSAGE
         }
         files = {"photo": photo_file}
-
         r = requests.post(url, data=payload, files=files)
         print("Send status:", r.status_code, r.text)
 
+def wait_and_send():
+    sent = False
+
+    while True:
+        now = datetime.now(ZoneInfo("Europe/Copenhagen"))
+
+        target = now.replace(
+            hour=22,
+            minute=0,
+            second=0,
+            microsecond=0
+        )
+
+        if not sent and target <= now < target.replace(minute=1):
+            send_message()
+            sent = True
+            break
+
+        time.sleep(10)
+
 if __name__ == "__main__":
-    print("Створюємо заплановане повідомлення...")
-    send_scheduled_message()
+    print("Тестовий бот, очікуємо 22:00 Данія...")
+    wait_and_send()
