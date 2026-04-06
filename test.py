b11433b5b2ffba7dca5e4d4e6b03e2e948a9b3d5
Kiril <kiril.strykalenko@icloud.com>
2026-04-06 21:13:34 +0200

    new type test

--- test.py
+++ test.py
@@ -1,42 +1,41 @@
 import requests
-import datetime
-import time
+from datetime import datetime
+from zoneinfo import ZoneInfo
 
 TOKEN = "8581090817:AAFC1bzXTfJvqHHxFmPzfXECciSjrErbyjM"
 CHAT_USERNAME = "@asdqwesszx"
-MESSAGE = "TEST Зупинімося на мить, щоб вшанувати пам’ять Героїв, які поклали своє життя заради нашого майбутнього. Їхній подвиг назавжди залишиться в нашій пам’яті. \n\nВічна Слава і вдячність."
-PHOTO_PATH = "photo.png"  # твій файл фото
+MESSAGE = "Зупинімося на мить, щоб вшанувати пам’ять Героїв, які поклали своє життя заради нашого майбутнього. Їхній подвиг назавжди залишиться в нашій пам’яті. \n\nВічна Слава і вдячність."
+PHOTO_PATH = "photo.png"
 
-TARGET_HOUR = 5  
-TARGET_MINUTE = 0
+def send_scheduled_message():
+    # час Києва
+    kyiv_time = datetime.now(ZoneInfo("Europe/Kyiv")).replace(
+        hour=9,
+        minute=0,
+        second=0,
+        microsecond=0
+    )
 
-def send_message():
+    # якщо вже пройшло 9:00 — ставимо на завтра
+    now_kyiv = datetime.now(ZoneInfo("Europe/Kyiv"))
+    if now_kyiv >= kyiv_time:
+        kyiv_time = kyiv_time.replace(day=kyiv_time.day + 1)
+
+    timestamp = int(kyiv_time.timestamp())
+
     url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
+
     with open(PHOTO_PATH, "rb") as photo_file:
         payload = {
             "chat_id": CHAT_USERNAME,
-            "caption": MESSAGE
+            "caption": MESSAGE,
+            "schedule_date": timestamp
         }
         files = {"photo": photo_file}
+
         r = requests.post(url, data=payload, files=files)
         print("Send status:", r.status_code, r.text)
 
-def wait_until_target():
-    while True:
-        now = datetime.datetime.now()
-        target = now.replace(
-            hour=TARGET_HOUR,
-            minute=TARGET_MINUTE,
-            second=0,
-            microsecond=0
-        )
-
-        if now >= target:
-            send_message()
-            break
-
-        time.sleep(5)  # перевірка кожні 5 секунд
-
 if __name__ == "__main__":
-    print("Бот запущений, очікуємо до 9:00...")
-    wait_until_target()
+    print("Створюємо заплановане повідомлення...")
+    send_scheduled_message()
