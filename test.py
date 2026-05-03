7e34f10c18b3fa6f725cbdbd07fa93e9831de79d
Kiril <kiril.strykalenko@icloud.com>
2026-05-03 14:07:29 +0200

    change test bot

--- test.py
+++ test.py
@@ -1,14 +1,12 @@
+from flask import Flask
 import requests
-import datetime
-import time
+
+app = Flask(__name__)
 
 TOKEN = "8581090817:AAFC1bzXTfJvqHHxFmPzfXECciSjrErbyjM"
 CHAT_USERNAME = "@asdqwesszx"
-MESSAGE = "Зупинімося на мить, щоб вшанувати пам’ять Героїв, які поклали своє життя заради нашого майбутнього. Їхній подвиг назавжди залишиться в нашій пам’яті. \n\nВічна Слава і вдячність."
-PHOTO_PATH = "photo.png"  # твій файл фото
-
-TARGET_HOUR = 21  
-TARGET_MINUTE = 0
+MESSAGE = "Зупинімося на мить, щоб вшанувати пам’ять Героїв, які поклали своє життя заради нашого майбутнього. Їхній подвиг назавжди залишиться в нашій пам’яті.\n\nВічна Слава і вдячність."
+PHOTO_PATH = "photo.png"
 
 def send_message():
     url = f"https://api.telegram.org/bot{TOKEN}/sendPhoto"
@@ -19,24 +17,16 @@ def send_message():
         }
         files = {"photo": photo_file}
         r = requests.post(url, data=payload, files=files)
-        print("Send status:", r.status_code, r.text)
+        print(r.status_code, r.text)
 
-def wait_until_target():
-    while True:
-        now = datetime.datetime.now()
-        target = now.replace(
-            hour=TARGET_HOUR,
-            minute=TARGET_MINUTE,
-            second=0,
-            microsecond=0
-        )
+@app.route("/send")
+def send():
+    send_message()
+    return "OK"
 
-        if now >= target:
-            send_message()
-            break
-
-        time.sleep(5)  # перевірка кожні 5 секунд
+@app.route("/")
+def home():
+    return "Bot is running"
 
 if __name__ == "__main__":
-    print("Бот запущений, очікуємо до 9:00...")
-    wait_until_target()
+    app.run()
